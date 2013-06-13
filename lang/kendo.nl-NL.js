39e1165f472ce2c9fbd4bdfb3839ec62815a3090
Mark Lagendijk <mark.lagendijk@backupagent.com>
2013-06-13 16:01:49 +0200

    Simplified the locale file format, and applied it to the English (en-US) and Dutch (nl-NL) locale. The advantages of this format are: - 1 object with all the labels. This object is in valid JSON format (quotes around propertynames). Because of this the object can be used directly in JSON resource files. - Easier to read.

--- lang/kendo.nl-NL.js
+++ lang/kendo.nl-NL.js
@@ -21,175 +21,158 @@
 */
 
 kendo.ui.Locale = "Nederlands (nl-NL)";
-kendo.ui.ColumnMenu.prototype.options.messages = 
-  $.extend(kendo.ui.ColumnMenu.prototype.options.messages, {
 
-/* COLUMN MENU MESSAGES 
- ****************************************************************************/   
-  "sortAscending": "Sorteer Oplopend",
-  "sortDescending": "Sorteer Aflopend",
-  "filter": "Filteren",
-  "columns": "Kolommen"
- /***************************************************************************/   
-});
+var labels = {
+	"ColumnMenu": {
+		"messages": {
+			"sortAscending": "Sorteer Oplopend",
+			"sortDescending": "Sorteer Aflopend",
+			"filter": "Filteren",
+			"columns": "Kolommen"
+		}
+	},
+	"Groupable": {
+		"messages": {
+			"empty": "Sleep een kolomkop hierheen om te groeperen op die kolom"
+		}
+	},
+	"FilterMenu": {
+		"messages": {
+			"info": "Toon items met een waarde die:",
+			"isTrue": "is waar",
+			"isFalse": "is niet waar",
+			"filter": "Filteren",
+			"clear": "Leegmaken",
+			"and": "En",
+			"or": "Of",
+			"selectValue": "-Selecteer waarde-"
+		},
+		"operators": {
+			"string": {
+				"eq": "Is gelijk aan",
+				"neq": "Is niet gelijk aan",
+				"startswith": "Begint met",
+				"contains": "Bevat",
+				"doesnotcontain": "Bevat niet",
+				"endswith": "Eindigt met"
+			},
+			"number": {
+				"eq": "Is gelijk aan",
+				"neq": "Is niet gelijk aan",
+				"gte": "Is groter dan of gelijk aan",
+				"gt": "Is groter dan",
+				"lte": "Is kleiner dan of gelijk aan",
+				"lt": "Is kleiner dan"
+			},
+			"date": {
+				"eq": "Is gelijk aan",
+				"neq": "Is niet gelijk aan",
+				"gte": "Is na of op",
+				"gt": "Is na",
+				"lte": "Is voor of op",
+				"lt": "Is voor"
+			},
+			"enums": {
+				"eq": "Is gelijk aan",
+				"neq": "Is niet gelijk aan"
+			}
+		}
+	},
+	"Pager": {
+		"messages": {
+			"display": "{0} - {1} van {2} items",
+			"empty": "Geen items om weer te geven",
+			"page": "Pagina",
+			"of": "van {0}",
+			"itemsPerPage": "items per pagina",
+			"first": "Ga naar de eerste pagina",
+			"previous": "Ga naar de vorige pagina",
+			"next": "Ga naar de volgende pagina",
+			"last": "Ga naar de laatste pagina",
+			"refresh": "Verversen"
+		}
+	},
+	"Validator": {
+		"messages": {
+			"required": "{0} is verplicht",
+			"pattern": "{0} is niet geldig",
+			"min": "{0} moet groter zijn dan of gelijk aan {1}",
+			"max": "{0} moet kleiner zijn dan of gelijk aan {1}",
+			"step": "{0} is niet geldig",
+			"email": "{0} is geen geldig emailadres",
+			"url": "{0} is geen geldige URL",
+			"date": "{0} is geen geldige datum"
+		}
+	},
+	"ImageBrowser": {
+		"messages": {
+			"uploadFile": "Uploaden",
+			"orderBy": "Sorteren op",
+			"orderByName": "Naam",
+			"orderBySize": "Grootte",
+			"directoryNotFound": "Er is geen map met deze naam gevonden.",
+			"emptyFolder": "Lege map",
+			"deleteFile": "Weet u zeker dat u \"{0}\" wilt verwijderen?",
+			"invalidFileType": "Het geselecteerde bestand \"{0}\" is ongeldig. De volgende bestandstypes worden ondersteund {1}.",
+			"overwriteFile": "Er bestaat al een bestand met de naam \"{0}\" in de huidige map. Wilt u dit bestand overschrijven?",
+			"dropFilesHere": "sleep bestanden hierheen om te uploaden"
+		}
+	},
+	"Editor": {
+		"messages": {
+			"bold": "Vet",
+			"italic": "Cursief",
+			"underline": "Onderstrepen",
+			"strikethrough": "Doorstrepen",
+			"superscript": "Superscript",
+			"subscript": "Subscript",
+			"justifyCenter": "Tekst centreren",
+			"justifyLeft": "Tekst links uitlijnen",
+			"justifyRight": "Tekst rechts uitlijnen",
+			"justifyFull": "Uitvullen",
+			"insertUnorderedList": "Opsommingstekens invoegen",
+			"insertOrderedList": "Nummering invoegen",
+			"indent": "Inspringen",
+			"outdent": "Uitspringen",
+			"createLink": "Hyperlink invoegen",
+			"unlink": "Hyperlink verwijderen",
+			"insertImage": "Afbeelding invoegen",
+			"insertHtml": "HTML invoegen",
+			"fontName": "Selecteer lettertype",
+			"fontNameInherit": "(lettertype overerven)",
+			"fontSize": "Selecteer lettergrootte",
+			"fontSizeInherit": "(lettergrootte overerven)",
+			"formatBlock": "Format",
+			"foreColor": "Tekstkleur",
+			"backColor": "Achtergrondkleur",
+			"style": "Stijlen",
+			"emptyFolder": "Lege Map",
+			"uploadFile": "Uploaden",
+			"orderBy": "Sorteer op:",
+			"orderBySize": "Grootte",
+			"orderByName": "Naam",
+			"invalidFileType": "Het geselecteerde bestand \"{0}\" is ongeldig. De volgende bestandstypes worden ondersteund {1}.",
+			"deleteFile": "Weet u zeker dat u \"{0}\" wilt verwijderen?",
+			"overwriteFile": "Er bestaat al een bestand met de naam \"{0}\" in de huidige map. Wilt u dit bestand overschrijven?",
+			"directoryNotFound": "Er is geen map met deze naam gevonden.",
+			"imageWebAddress": "Web adres",
+			"imageAltText": "Alternatieve tekst",
+			"dialogInsert": "Invoegen",
+			"dialogButtonSeparator": "of",
+			"dialogCancel": "Annuleren"
+		}
+	}
+};
 
-kendo.ui.Groupable.prototype.options.messages = 
-  $.extend(kendo.ui.Groupable.prototype.options.messages, {
-
-/* GRID GROUP PANEL MESSAGES 
- ****************************************************************************/   
-  "empty": "Sleep een kolomkop hierheen om te groeperen op die kolom"
- /***************************************************************************/   
-});
-
-kendo.ui.FilterMenu.prototype.options.messages = 
-  $.extend(kendo.ui.FilterMenu.prototype.options.messages, {
-  
-/* FILTER MENU MESSAGES 
- ***************************************************************************/   
-  "info": "Toon items met een waarde die:",
-  "isTrue": "is waar",
-  "isFalse": "is niet waar",
-  "filter": "Filteren",
-  "clear": "Leegmaken",
-  "and": "En",
-  "or": "Of",
-  "selectValue": "-Selecteer waarde-"
- /***************************************************************************/   
-});
-         
-kendo.ui.FilterMenu.prototype.options.operators =           
-  $.extend(kendo.ui.FilterMenu.prototype.options.operators, {
-
-/* FILTER MENU OPERATORS (for each supported data type) 
- ****************************************************************************/   
-  "string": {
-    "eq": "Is gelijk aan",
-    "neq": "Is niet gelijk aan",
-    "startswith": "Begint met",
-    "contains": "Bevat",
-    "doesnotcontain": "Bevat niet",
-    "endswith": "Eindigt met"
-  },
-  "number": {
-    "eq": "Is gelijk aan",
-    "neq": "Is niet gelijk aan",
-    "gte": "Is groter dan of gelijk aan",
-    "gt": "Is groter dan",
-    "lte": "Is kleiner dan of gelijk aan",
-    "lt": "Is kleiner dan"
-  },
-  "date": {
-    "eq": "Is gelijk aan",
-    "neq": "Is niet gelijk aan",
-    "gte": "Is na of op",
-    "gt": "Is na",
-    "lte": "Is voor of op",
-    "lt": "Is voor"
-  },
-  "enums": {
-    "eq": "Is gelijk aan",
-    "neq": "Is niet gelijk aan"
-  }
- /***************************************************************************/   
-});
-
-kendo.ui.Pager.prototype.options.messages = 
-  $.extend(kendo.ui.Pager.prototype.options.messages, {
-  
-/* PAGER MESSAGES 
- ****************************************************************************/   
-  "display": "{0} - {1} van {2} items",
-  "empty": "Geen items om weer te geven",
-  "page": "Pagina",
-  "of": "van {0}",
-  "itemsPerPage": "items per pagina",
-  "first": "Ga naar de eerste pagina",
-  "previous": "Ga naar de vorige pagina",
-  "next": "Ga naar de volgende pagina",
-  "last": "Ga naar de laatste pagina",
-  "refresh": "Verversen"
- /***************************************************************************/   
-});
-
-kendo.ui.Validator.prototype.options.messages = 
-  $.extend(kendo.ui.Validator.prototype.options.messages, {
-
-/* VALIDATOR MESSAGES 
- ****************************************************************************/   
-  "required": "{0} is verplicht",
-  "pattern": "{0} is niet geldig",
-  "min": "{0} moet groter zijn dan of gelijk aan {1}",
-  "max": "{0} moet kleiner zijn dan of gelijk aan {1}",
-  "step": "{0} is niet geldig",
-  "email": "{0} is geen geldig emailadres",
-  "url": "{0} is geen geldige URL",
-  "date": "{0} is geen geldige datum"
- /***************************************************************************/   
-});
-
-kendo.ui.ImageBrowser.prototype.options.messages = 
-  $.extend(kendo.ui.ImageBrowser.prototype.options.messages, {
-
-/* IMAGE BROWSER MESSAGES 
- ****************************************************************************/   
-  "uploadFile": "Uploaden",
-  "orderBy": "Sorteren op",
-  "orderByName": "Naam",
-  "orderBySize": "Grootte",
-  "directoryNotFound": "Er is geen map met deze naam gevonden.",
-  "emptyFolder": "Lege map",
-  "deleteFile": "Weet u zeker dat u \"{0}\" wilt verwijderen?",
-  "invalidFileType": "Het geselecteerde bestand \"{0}\" is ongeldig. De volgende bestandstypes worden ondersteund {1}.",
-  "overwriteFile": "Er bestaat al een bestand met de naam \"{0}\" in de huidige map. Wilt u dit bestand overschrijven?",
-  "dropFilesHere": "sleep bestanden hierheen om te uploaden"
- /***************************************************************************/   
-});
-
-kendo.ui.Editor.prototype.options.messages = 
-  $.extend(kendo.ui.Editor.prototype.options.messages, {
-
-/* EDITOR MESSAGES 
- ****************************************************************************/   
-  "bold": "Vet",
-  "italic": "Cursief",
-  "underline": "Onderstrepen",
-  "strikethrough": "Doorstrepen",
-  "superscript": "Superscript",
-  "subscript": "Subscript",
-  "justifyCenter": "Tekst centreren",
-  "justifyLeft": "Tekst links uitlijnen",
-  "justifyRight": "Tekst rechts uitlijnen",
-  "justifyFull": "Uitvullen",
-  "insertUnorderedList": "Opsommingstekens invoegen",
-  "insertOrderedList": "Nummering invoegen",
-  "indent": "Inspringen",
-  "outdent": "Uitspringen",
-  "createLink": "Hyperlink invoegen",
-  "unlink": "Hyperlink verwijderen",
-  "insertImage": "Afbeelding invoegen",
-  "insertHtml": "HTML invoegen",
-  "fontName": "Selecteer lettertype",
-  "fontNameInherit": "(lettertype overerven)",
-  "fontSize": "Selecteer lettergrootte",
-  "fontSizeInherit": "(lettergrootte overerven)",
-  "formatBlock": "Format",
-  "foreColor": "Tekstkleur",
-  "backColor": "Achtergrondkleur",
-  "style": "Stijlen",
-  "emptyFolder": "Lege Map",
-  "uploadFile": "Uploaden",
-  "orderBy": "Sorteer op:",
-  "orderBySize": "Grootte",
-  "orderByName": "Naam",
-  "invalidFileType": "Het geselecteerde bestand \"{0}\" is ongeldig. De volgende bestandstypes worden ondersteund {1}.",
-  "deleteFile": "Weet u zeker dat u \"{0}\" wilt verwijderen?",
-  "overwriteFile": "Er bestaat al een bestand met de naam \"{0}\" in de huidige map. Wilt u dit bestand overschrijven?",
-  "directoryNotFound": "Er is geen map met deze naam gevonden.",
-  "imageWebAddress": "Web adres",
-  "imageAltText": "Alternatieve tekst",
-  "dialogInsert": "Invoegen",
-  "dialogButtonSeparator": "of",
-  "dialogCancel": "Annuleren"
- /***************************************************************************/   
-});
+// Loop through all Components
+for(var componentName in labels){
+	var component = labels[componentName];
+	
+	// Loop through all options within that component (eg: 'messages' or 'operators)
+	for(var optionName in component){
+		var optionLabels = component[optionName];
+		
+		// Extend the KendoUI labels for the current option
+		$.extend(kendo.ui[componentName].prototype.options[optionName], optionLabels);
+	}
+}
