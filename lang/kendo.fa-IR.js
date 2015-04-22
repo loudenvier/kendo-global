656ba288a70a0110589dd47542c83160eb273ed4
Salar Khalilzadeh <salar2k@gmail.com>
2015-04-22 14:47:36 +0430

    Update kendo.fa-IR.js
    Kendo Grid messages are localized.

--- lang/kendo.fa-IR.js
+++ lang/kendo.fa-IR.js
@@ -21,177 +21,201 @@
 * For GPL requirements, please review: http://www.gnu.org/copyleft/gpl.html
 */
 kendo.ui.Locale = "Persian (fa-IR)";
-kendo.ui.ColumnMenu.prototype.options.messages =
-  $.extend(kendo.ui.ColumnMenu.prototype.options.messages, {
 
-      /* COLUMN MENU MESSAGES 
-       ****************************************************************************/
-      sortAscending: "مرتب سازی صعودی",
-      sortDescending: "مرتب سازی نزولی",
-      filter: "فیلتر",
-      columns: "ستون ها"
-      /***************************************************************************/
-  });
+kendo.ui.Grid.prototype.options.messages =
+	$.extend(kendo.ui.Grid.prototype.options.messages, {
+		commands: {
+			cancel: "لغو تغییرات",
+			canceledit: "انصراف",
+			create: "ایجاد رکورد جدید",
+			destroy: "حذف",
+			edit: "ویرایش",
+			excel: "خروجی اکسل",
+			pdf: "خروجی PDF",
+			save: "ذخیره تغییرات",
+			update: "به روزرسانی"
+		},
+		editable: {
+			cancelDelete: "انصراف",
+			confirmDelete: "حذف",
+			confirmation: "آیا از حذف این رکورد مطمئن هستید؟"
+		}
+	});
+kendo.ui.ColumnMenu.prototype.options.messages =
+	$.extend(kendo.ui.ColumnMenu.prototype.options.messages, {
+
+		/* COLUMN MENU MESSAGES 
+	   ****************************************************************************/
+		sortAscending: "مرتب سازی صعودی",
+		sortDescending: "مرتب سازی نزولی",
+		filter: "فیلتر",
+		columns: "ستون ها",
+		lock: "قفل کردن",
+		done: "تمام",
+		unlock: "بارکردن قفل"
+		/***************************************************************************/
+	});
 
 kendo.ui.Groupable.prototype.options.messages =
   $.extend(kendo.ui.Groupable.prototype.options.messages, {
 
-      /* GRID GROUP PANEL MESSAGES 
-       ****************************************************************************/
-      empty: "ستون ها را جهت گروه بندی در اینجا قرار دهید"
-      /***************************************************************************/
+	/* GRID GROUP PANEL MESSAGES 
+	 ****************************************************************************/
+	empty: "ستون ها را جهت گروه بندی در اینجا قرار دهید"
+	/***************************************************************************/
   });
 
 kendo.ui.FilterMenu.prototype.options.messages =
   $.extend(kendo.ui.FilterMenu.prototype.options.messages, {
 
-      /* FILTER MENU MESSAGES 
-       ***************************************************************************/
-      info: "نشان دادن مواردی که:",        // sets the text on top of the filter menu
-      filter: "فیلتر",      // sets the text for the "Filter" button
-      clear: "پاک کردن",        // sets the text for the "Clear" button
-      // when filtering boolean numbers
-      isTrue: "درست باشد", // sets the text for "isTrue" radio button
-      isFalse: "نادرست باشد",     // sets the text for "isFalse" radio button
-      //changes the text of the "And" and "Or" of the filter menu
-      and: "و",
-      or: "یا",
-      selectValue: "-انتخاب کنید-"
-      /***************************************************************************/
+	/* FILTER MENU MESSAGES 
+	 ***************************************************************************/
+	info: "نشان دادن مواردی که:",        // sets the text on top of the filter menu
+	filter: "فیلتر",      // sets the text for the "Filter" button
+	clear: "پاک کردن",        // sets the text for the "Clear" button
+	// when filtering boolean numbers
+	isTrue: "درست باشد", // sets the text for "isTrue" radio button
+	isFalse: "نادرست باشد",     // sets the text for "isFalse" radio button
+	//changes the text of the "And" and "Or" of the filter menu
+	and: "و",
+	or: "یا",
+	selectValue: "-انتخاب کنید-"
+	/***************************************************************************/
   });
 
 kendo.ui.FilterMenu.prototype.options.operators =
   $.extend(kendo.ui.FilterMenu.prototype.options.operators, {
 
-      /* FILTER MENU OPERATORS (for each supported data type) 
-       ****************************************************************************/
-      string: {
-          eq: "برابر",
-          neq: "مخالف",
-          startswith: "شروع می شوند",
-          contains: "دارا می باشند",
-          doesnotcontain: "دارا نمی باشند",
-          endswith: "خاتمه می یابند"
-      },
-      number: {
-          eq: "مساوی",
-          neq: "مخالف",
-          gte: "بزرگتر یا مساوی",
-          gt: "بزگتر",
-          lte: "کوچکتر یا مساوی",
-          lt: "کوچکتر"
-      },
-      date: {
-          eq: "مساوی",
-          neq: "مخالف",
-          gte: "بزرگتر یا مساوی",
-          gt: "بزگتر",
-          lte: "کوچکتر یا مساوی",
-          lt: "کوچکتر"
-      },
-      enums: {
-          eq: "برابر",
-          neq: "مخالف"
-      }
-      /***************************************************************************/
+	/* FILTER MENU OPERATORS (for each supported data type) 
+	 ****************************************************************************/
+	string: {
+		eq: "برابر",
+		neq: "مخالف",
+		startswith: "شروع می شوند",
+		contains: "دارا می باشند",
+		doesnotcontain: "دارا نمی باشند",
+		endswith: "خاتمه می یابند"
+	},
+	number: {
+		eq: "مساوی",
+		neq: "مخالف",
+		gte: "بزرگتر یا مساوی",
+		gt: "بزگتر",
+		lte: "کوچکتر یا مساوی",
+		lt: "کوچکتر"
+	},
+	date: {
+		eq: "مساوی",
+		neq: "مخالف",
+		gte: "بزرگتر یا مساوی",
+		gt: "بزگتر",
+		lte: "کوچکتر یا مساوی",
+		lt: "کوچکتر"
+	},
+	enums: {
+		eq: "برابر",
+		neq: "مخالف"
+	}
+	/***************************************************************************/
   });
 
 kendo.ui.Pager.prototype.options.messages =
   $.extend(kendo.ui.Pager.prototype.options.messages, {
 
-      /* PAGER MESSAGES 
-       ****************************************************************************/
-      display: "{0} - {1} از {2} مورد",
-      empty: "موردی یافت نشد",
-      page: "صفحه",
-      of: "از {0}",
-      itemsPerPage: "تعداد موارد در هر صفحه",
-      first: "اولین",
-      previous: "قبلی",
-      next: "بعدی",
-      last: "آخرین",
-      refresh: "بازنشانی"
-      /***************************************************************************/
-  });
+	/* PAGER MESSAGES 
+	 ****************************************************************************/
+	display: "{0} - {1} از {2} مورد",
+	empty: "موردی یافت نشد",
+	page: "صفحه",
+	of: "از {0}",
+	itemsPerPage: "تعداد موارد در هر صفحه",
+	first: "اولین",
+	previous: "قبلی",
+	next: "بعدی",
+	last: "آخرین",
+	refresh: "بازنشانی",
+	morePages: "صفحات بیشتر"
+	/***************************************************************************/
+});
 
 kendo.ui.Validator.prototype.options.messages =
   $.extend(kendo.ui.Validator.prototype.options.messages, {
 
-      /* VALIDATOR MESSAGES 
-       ****************************************************************************/
-      required: "وارد نمودن {0} الزامی است.",
-      pattern: "{0} را صحیح وارد نمائید.",
-      min: "{0} باید بزرگتر از {1} باشد",
-      max: "{0} باید کوچکتر از {1} باشد",
-      step: "{0} صحیح نمی باشد.",
-      email: "{0} به عنوان آدرس ایمیل صحیح وارد نشده است.",
-      url: "{0} به عنوان آدرس اینترنتی صحیح وارد نشده است",
-      date: "{0} به عنوان تاریخ صحیح وارد نشده است"
-      /***************************************************************************/
-  });
+	/* VALIDATOR MESSAGES 
+	 ****************************************************************************/
+		required: "وارد نمودن {0} الزامی است.",
+		pattern: "{0} را صحیح وارد نمائید.",
+		min: "{0} باید بزرگتر از {1} باشد",
+		max: "{0} باید کوچکتر از {1} باشد",
+		step: "{0} صحیح نمی باشد.",
+		email: "{0} به عنوان آدرس ایمیل صحیح وارد نشده است.",
+		url: "{0} به عنوان آدرس اینترنتی صحیح وارد نشده است",
+		date: "{0} به عنوان تاریخ صحیح وارد نشده است"
+	/***************************************************************************/
+});
 
 kendo.ui.ImageBrowser.prototype.options.messages =
   $.extend(kendo.ui.ImageBrowser.prototype.options.messages, {
 
-      /* IMAGE BROWSER MESSAGES 
-       ****************************************************************************/
-      uploadFile: "بارگذاری فایل",
-      orderBy: "مرتب سازی با",
-      orderByName: "مرتب سازی با نام",
-      orderBySize: "مرتب سازی با اندازه",
-      directoryNotFound: "مسیر مورد نظر یافت نشد.",
-      emptyFolder: "خالی نمودن پوشه",
-      deleteFile: 'آیا مطمئن هستید که "{0}" پاک شود؟',
-      invalidFileType: "فایل انتخاب شده \"{0}\" نامعتبر است. فایل های پشتیبانی شده عبارتند از: {1}.",
-      overwriteFile: "فایل با نام \"{0}\" در مسیر مورد نظر وجود دارد. روی آن نوشته شود؟",
-      dropFilesHere: "فایل ها را اینجا قرار دهید"
-      /***************************************************************************/
-  });
+	/* IMAGE BROWSER MESSAGES 
+	 ****************************************************************************/
+		uploadFile: "بارگذاری فایل",
+		orderBy: "مرتب سازی با",
+		orderByName: "مرتب سازی با نام",
+		orderBySize: "مرتب سازی با اندازه",
+		directoryNotFound: "مسیر مورد نظر یافت نشد.",
+		emptyFolder: "خالی نمودن پوشه",
+		deleteFile: 'آیا مطمئن هستید که "{0}" پاک شود؟',
+		invalidFileType: "فایل انتخاب شده \"{0}\" نامعتبر است. فایل های پشتیبانی شده عبارتند از: {1}.",
+		overwriteFile: "فایل با نام \"{0}\" در مسیر مورد نظر وجود دارد. روی آن نوشته شود؟",
+		dropFilesHere: "فایل ها را اینجا قرار دهید"
+	/***************************************************************************/
+});
 
 kendo.ui.Editor.prototype.options.messages =
   $.extend(kendo.ui.Editor.prototype.options.messages, {
 
-      /* EDITOR MESSAGES 
-       ****************************************************************************/
-      bold: "پررنگ",
-      italic: "مورب",
-      underline: "زیرخط",
-      strikethrough: "strikethrough",
-      superscript: "بالانویس",
-      subscript: "زیرنویس",
-      justifyCenter: "مرتب سازی به مرکز",
-      justifyLeft: "مرتب سازی به چپ",
-      justifyRight: "مرتب سازی به راست",
-      justifyFull: "مرتب سازی کامل",
-      insertUnorderedList: "درج لیست نامرتب",
-      insertOrderedList: "درج لیست مرتب",
-      indent: "افزایش فاصله",
-      outdent: "کاهش فاصله",
-      createLink: "ایجاد پیوند",
-      unlink: "حذف پیوند",
-      insertImage: "درج تصویر",
-      insertHtml: "درج HTML",
-      fontName: "نام قلم",
-      fontNameInherit: "قلم",
-      fontSize: "اندازه قلم",
-      fontSizeInherit: "اندازه قلم",
-      formatBlock: "قالب دهی",
-      foreColor: "رنگ",
-      backColor: "رنگ پس زمینه",
-      style: "طرح",
-      emptyFolder: "خالی نمودن مسیر",
-      uploadFile: "بارگذاری فایل",
-      orderBy: "مرتب سازی با:",
-      orderBySize: "مرتب سازی بر اساس اندازه",
-      orderByName: "مرتب سازی بر اساس نام",
-      invalidFileType: "فایل انتخاب شده\"{0}\" نامعتبر است. فایل های پشتیبانی شده عبارتند از: {1}.",
-      deleteFile: 'آیا مطمئن هستید که  "{0}" پاک شود؟',
-      overwriteFile: "فایل با نام \"{0}\" در مسیر مورد نظر وجود دارد. روی آن نوشته شود؟",
-      directoryNotFound: "مسیر مورد نظر یافت نشد",
-      imageWebAddress: "آدرس اینترنتی تصویر",
-      imageAltText: "متن جایگزین",
-      dialogInsert: "درج",
-      dialogButtonSeparator: "یا",
-      dialogCancel: "انصراف"
-      /***************************************************************************/
-  });
+	/* EDITOR MESSAGES 
+	 ****************************************************************************/
+		bold: "پررنگ",
+		italic: "مورب",
+		underline: "زیرخط",
+		strikethrough: "strikethrough",
+		superscript: "بالانویس",
+		subscript: "زیرنویس",
+		justifyCenter: "مرتب سازی به مرکز",
+		justifyLeft: "مرتب سازی به چپ",
+		justifyRight: "مرتب سازی به راست",
+		justifyFull: "مرتب سازی کامل",
+		insertUnorderedList: "درج لیست نامرتب",
+		insertOrderedList: "درج لیست مرتب",
+		indent: "افزایش فاصله",
+		outdent: "کاهش فاصله",
+		createLink: "ایجاد پیوند",
+		unlink: "حذف پیوند",
+		insertImage: "درج تصویر",
+		insertHtml: "درج HTML",
+		fontName: "نام قلم",
+		fontNameInherit: "قلم",
+		fontSize: "اندازه قلم",
+		fontSizeInherit: "اندازه قلم",
+		formatBlock: "قالب دهی",
+		foreColor: "رنگ",
+		backColor: "رنگ پس زمینه",
+		style: "طرح",
+		emptyFolder: "خالی نمودن مسیر",
+		uploadFile: "بارگذاری فایل",
+		orderBy: "مرتب سازی با:",
+		orderBySize: "مرتب سازی بر اساس اندازه",
+		orderByName: "مرتب سازی بر اساس نام",
+		invalidFileType: "فایل انتخاب شده\"{0}\" نامعتبر است. فایل های پشتیبانی شده عبارتند از: {1}.",
+		deleteFile: 'آیا مطمئن هستید که  "{0}" پاک شود؟',
+		overwriteFile: "فایل با نام \"{0}\" در مسیر مورد نظر وجود دارد. روی آن نوشته شود؟",
+		directoryNotFound: "مسیر مورد نظر یافت نشد",
+		imageWebAddress: "آدرس اینترنتی تصویر",
+		imageAltText: "متن جایگزین",
+		dialogInsert: "درج",
+		dialogButtonSeparator: "یا",
+		dialogCancel: "انصراف"
+	/***************************************************************************/
+});
